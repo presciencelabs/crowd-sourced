c5393f65ccc800d3ed7f8167163cd8395685d9b3
Brett Palmberg <>
2024-05-01 21:32:13 -0500

    Add 'X' Close Icon in Upper-Right-Hand Corner

--- ui/app.js
+++ ui/app.js
@@ -6,6 +6,7 @@ import { Header } from "./components/header";
 import { AudioInput } from "./components/input-audio";
 import { TextInput } from "./components/input-text";
 import { LanguageInput } from "./components/input-language";
+import { XMarkIcon } from "@heroicons/react/20/solid";
 
 function handleSubmit(e, payload) {
   const f = new FormData();
@@ -25,6 +26,7 @@ function App(props) {
   const [audio, setAudio] = useState(null);
   const [text, setText] = useState(null);
   const [payload, setPayload] = useState({});
+  const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
     setPayload({
@@ -36,14 +38,15 @@ function App(props) {
   }, [img, audio, text, language]);
 
   return (
-    <>
-      <div
-        class="relative z-10"
-        aria-labelledby="modal-title"
-        role="dialog"
-        aria-modal="true"
-      >
-        {/* <!--
+    isOpen && (
+      <>
+        <div
+          class="relative z-10"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          {/* <!--
           Background backdrop, show/hide based on modal state.
       
           Entering: "ease-out duration-300"
@@ -53,11 +56,11 @@ function App(props) {
             From: "opacity-100"
             To: "opacity-0"
         --> */}
-      </div>
-      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          {/* <!--
+        </div>
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            {/* <!--
       Modal panel, show/hide based on modal state.
 
       Entering: "ease-out duration-300"
@@ -67,43 +70,53 @@ function App(props) {
         From: "opacity-100 translate-y-0 sm:scale-100"
         To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     --> */}
-          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              <Header language={language} />
+            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div class="flex flex-row-reverse">
+                  <button
+                    onClick={(e) => {
+                      setIsOpen(false);
+                    }}
+                  >
+                    <XMarkIcon class="h-8 w-8" />
+                  </button>
+                </div>
+                <Header language={language} />
 
-              {/* LANGUAGE SELECTOR */}
+                {/* LANGUAGE SELECTOR */}
 
-              {/* IMAGE APP */}
-              {/* START IMAGE */}
-              <div class="flex justify-around">
-                <img class="w-40 h-40" src={img.url} />
+                {/* IMAGE APP */}
+                {/* START IMAGE */}
+                <div class="flex justify-around">
+                  <img class="w-40 h-40" src={img.url} />
+                </div>
+                {/* END IMAGE */}
               </div>
-              {/* END IMAGE */}
-            </div>
-            {/* <!-- INPUT SECTION --> */}
-            <div class="flex flex-col space-y-3 px-4 sm:px-6">
-              <LanguageInput language={language} setLanguage={setLanguage} />
+              {/* <!-- INPUT SECTION --> */}
+              <div class="flex flex-col space-y-3 px-4 sm:px-6">
+                <LanguageInput language={language} setLanguage={setLanguage} />
 
-              {language && (
-                <>
-                  <TextInput text={text} setText={setText} />
-                  <AudioInput setAudio={setAudio} />
-                </>
-              )}
-            </div>
-            <div class="bg-gray-50 px-4 py-6 sm:flex sm:flex-row-reverse sm:px-6">
-              <button
-                type="button"
-                class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                onClick={(e) => handleSubmit(e, payload)}
-              >
-                Submit
-              </button>
+                {language && (
+                  <>
+                    <TextInput text={text} setText={setText} />
+                    <AudioInput setAudio={setAudio} />
+                  </>
+                )}
+              </div>
+              <div class="bg-gray-50 px-4 py-6 sm:flex sm:flex-row-reverse sm:px-6">
+                <button
+                  type="button"
+                  class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                  onClick={(e) => handleSubmit(e, payload)}
+                >
+                  Submit
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    )
   );
 }
 
